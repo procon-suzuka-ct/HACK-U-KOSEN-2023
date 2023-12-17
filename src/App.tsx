0ff71f2b696d7efc07fed585be10e24778b172c3
Giwa-shun <yamagiwashunta@icloud.com>
2023-12-17 22:34:56 +0900

    update:create confirmation page update;create finish page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home.tsx";
 import Payment from "./pages/Payment.tsx";
 import Design from "./pages/Design.tsx";
 import Login from "./pages/Login.tsx";
+import Confirmation from "./pages/Confirmation.tsx";
+import Finish from "./pages/finish.tsx";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +14,8 @@ function App() {
         <Route path={"/payment"} element={<Payment/>}/>
         <Route path={"/design"} element={<Design/>}/>
         <Route path={"/login"} element={<Login/>}/>
+        <Route path={"/confirmation"} element={<Confirmation/>}/>
+        <Route path={"/finish"} element={<Finish/>}/>
       </Routes>
     </BrowserRouter>
   )
