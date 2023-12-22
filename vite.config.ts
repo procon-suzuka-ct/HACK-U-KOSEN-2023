8b6823d170a95a9a65d6fd36fd67db01c86def16
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-12-22 23:11:55 +0900

    update: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {VitePWA, VitePWAOptions} from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const pwaConfig: VitePWAOptions = {
+  disable: false,
+  includeManifestIcons: false,
+  injectManifest: undefined,
+  injectRegister: undefined,
+  minify: false,
+  workbox: {
+    navigateFallbackDenylist: [/\/__\/auth/],
+    navigateFallbackAllowlist: [/^(?!\/__).*/],
+  },
+  registerType: 'autoUpdate',
+  includeAssets: ['icon.jpg'],
+  manifest: {
+    theme_color: "#FFFFFF",
+    background_color: "#FFFFFF",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    name: "けーきやさんた",
+    short_name: "けーきやさんた",
+    icons: [
+      {
+        src: "/icon.jpg",
+        sizes: "256x256",
+        type: "image/jpg",
+      }
+    ]
+  },
+  devOptions: {
+    enabled: true,
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA(pwaConfig),
+    react()],
 })
