6d312487cb96b9732fe0baa14d30456df59ac869
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-12-22 11:05:42 +0900

    update: change auth type popup to redirect

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,19 +5,30 @@ import {appUserStore} from "../stores/appUserStore.ts";
 import {useNavigate} from "react-router-dom";
 import {useStore} from "@nanostores/react";
 import {activePathStore} from "../stores/activePathStore.ts";
+import {getRedirectResult, GoogleAuthProvider, signInWithRedirect} from "firebase/auth";
+import {useEffect} from "react";
+import {auth} from "../firebase.ts";
 
 const Login = () => {
   const router = useNavigate();
   const activePath = useStore(activePathStore);
 
+  useEffect(() => {
+    getRedirectResult(auth).then((result) => {
+      if (result?.user) {
+        const newUser = new AppUser(result.user);
+        appUserStore.set(newUser);
+        router(activePath);
+      }
+    })
+  }, [activePath, router]);
+
   return (
     <div className={styles.container}>
       <p>Login</p>
-      <FilledButton onClick={() => {
-        AppUser.login().then((user) => {
-          appUserStore.set(user);
-          router(activePath);
-        })
+      <FilledButton onClick={async () => {
+        const provider = new GoogleAuthProvider();
+        await signInWithRedirect(auth, provider);
       }}>ログイン</FilledButton>
     </div>
   )
