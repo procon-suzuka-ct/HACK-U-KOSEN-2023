31640b1864758ee8f82c7c78e2803eae9cfc061f
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-12-22 12:49:45 +0900

    update: direct google login from Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Payment from "./pages/Payment.tsx";
 import Design from "./pages/Design.tsx";
-import Login from "./pages/Login.tsx";
-import Template from "./Template.tsx";
 import Confirmation from "./pages/Confirmation.tsx";
 import OnlyCake from "./pages/OnlyCake.tsx";
 import Change from "./pages/Change.tsx";
@@ -13,18 +11,15 @@ import Finish from "./pages/Finish.tsx";
 function App() {
   return (
     <BrowserRouter>
-      <Template>
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/payment"} element={<Payment />} />
-          <Route path={"/design"} element={<Design />} />
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/onlycake"} element={<OnlyCake />} />
-          <Route path={"/confirmation"} element={<Confirmation />} />
-          <Route path={"/change"} element={<Change />} />
-          <Route path={"/finish"} element={<Finish />} />
-        </Routes>
-      </Template>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/payment"} element={<Payment />} />
+        <Route path={"/design"} element={<Design />} />
+        <Route path={"/onlycake"} element={<OnlyCake />} />
+        <Route path={"/confirmation"} element={<Confirmation />} />
+        <Route path={"/change"} element={<Change />} />
+        <Route path={"/finish"} element={<Finish />} />
+      </Routes>
     </BrowserRouter>
   )
 }
